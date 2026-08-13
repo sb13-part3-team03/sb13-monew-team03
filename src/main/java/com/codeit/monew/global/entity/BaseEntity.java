a4365fb4e33ab91f55b345c74f2bac82ae3ecd49
줴령 <gpfud09@gmail.com>
2026-08-13 19:21:06 +0900

    refactor: BaseEntity UUID import 수정

--- src/main/java/com/codeit/monew/global/entity/BaseEntity.java
+++ src/main/java/com/codeit/monew/global/entity/BaseEntity.java
@@ -9,7 +9,7 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.UUID;
+import java.util.UUID;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
