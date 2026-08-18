c80b2d6325ccba7b5d9be7739c688dcd984e074a
줴령 <gpfud09@gmail.com>
2026-08-18 17:19:27 +0900

    feat: ArticleQueryServiceImpl 검색 조회 구현

--- src/main/java/com/codeit/monew/article/service/ArticleQueryServiceImpl.java
+++ src/main/java/com/codeit/monew/article/service/ArticleQueryServiceImpl.java
@@ -1,12 +1,15 @@
 package com.codeit.monew.article.service;
 
 import com.codeit.monew.article.dto.request.ArticleSearchRequest;
+import com.codeit.monew.article.dto.response.ArticleDto;
+import com.codeit.monew.article.dto.response.ArticleSearchResult;
 import com.codeit.monew.article.dto.response.CursorPageResponseArticleDto;
 import com.codeit.monew.article.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -17,7 +20,65 @@ public class ArticleQueryServiceImpl implements ArticleQueryService {
 
     @Override
     @Transactional(readOnly = true)
-    public CursorPageResponseArticleDto searchArticles(ArticleSearchRequest request, UUID userId) {
-        return null;
+    public CursorPageResponseArticleDto searchArticles(
+            ArticleSearchRequest request,
+            UUID userId
+    ) {
+        // limit + 1개 조회
+        List<ArticleSearchResult> results = articleRepository.searchArticles(request);
+
+        // 다음 페이지 존재 여부
+        boolean hasNext = results.size() > request.limit();
+
+        // 실제 응답 데이터
+        List<ArticleSearchResult> content = hasNext
+                ? results.subList(0, request.limit())
+                : results;
+
+        // 마지막 데이터
+        ArticleSearchResult last = content.isEmpty() ? null : content.get(content.size() - 1);
+
+        String nextCursor = null;
+        UUID nextAfter = null;
+
+        if (hasNext && last != null) {
+            nextCursor = createNextCursor(last, request.orderBy());
+
+            nextAfter = last.article().getId();
+        }
+
+        // 전체 검색 결과 개수
+        long totalElements = articleRepository.countTotalElements(request);
+
+        List<ArticleDto> articleDtos = content.stream()
+                .map(ArticleDto::from)
+                .toList();
+
+        return new CursorPageResponseArticleDto(
+                articleDtos,
+                nextCursor,
+                nextAfter,
+                request.limit(),
+                totalElements,
+                hasNext
+        );
     }
+
+    private String createNextCursor(
+            ArticleSearchResult result,
+            String orderBy
+    ) {
+        return switch (orderBy) {
+            case "commentCount" ->
+                    String.valueOf(result.commentCount());
+
+            case "viewCount" ->
+                    String.valueOf(result.viewCount());
+
+            default ->
+                    result.article().getPublishDate().toString();
+        };
+    }
+
+
 }
