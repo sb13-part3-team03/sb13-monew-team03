be1121216227948856f633b18f08a1926a497199
marcel615 <alswo000615@gmail.com>
2026-08-19 09:47:22 +0900

    feat: 관심사 API 기본 구조 구현 2
    - InterestController에 관심사 등록, 조회, 수정, 삭제 API 엔드포인트 작성
    - InterestController에 관심사 구독 및 구독 취소 API 엔드포인트 작성
    - Command/Condition 기반으로 Service 메서드 시그니처 구성

--- src/main/java/com/codeit/monew/interest/service/InterestService.java
+++ src/main/java/com/codeit/monew/interest/service/InterestService.java
@@ -1,11 +1,39 @@
 package com.codeit.monew.interest.service;
 
+import com.codeit.monew.interest.dto.response.CursorPageResponseInterestDto;
+import com.codeit.monew.interest.dto.response.InterestDto;
+import com.codeit.monew.interest.dto.response.SubscriptionDto;
+import com.codeit.monew.interest.service.command.*;
+import com.codeit.monew.interest.service.condition.InterestSearchCondition;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class InterestService {
+
+    public InterestDto createInterest(InterestRegisterCommand command) {
+        return null;
+    }
+
+    public SubscriptionDto subscribe(InterestSubscribeCommand command) {
+        return null;
+    }
+
+    public CursorPageResponseInterestDto findInterests(InterestSearchCondition condition) {
+        return null;
+    }
+
+    public InterestDto updateInterest(InterestUpdateCommand command) {
+        return null;
+    }
+
+    public void deleteInterest(InterestDeleteCommand command) {
+
+    }
+
+    public void unsubscribe(InterestUnsubscribeCommand command) {
+
+    }
 }
