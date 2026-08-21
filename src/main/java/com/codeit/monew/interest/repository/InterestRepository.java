6fcc5cc823f0227621e06e22f7f51d976ddba6d0
hajung <bover80@naver.com>
2026-08-21 10:10:04 +0900

    fix: 기사 수집 시 관심사 키워드 지연 로딩 오류 수정

--- src/main/java/com/codeit/monew/interest/repository/InterestRepository.java
+++ src/main/java/com/codeit/monew/interest/repository/InterestRepository.java
@@ -2,9 +2,18 @@ package com.codeit.monew.interest.repository;
 
 import com.codeit.monew.interest.entity.Interest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface InterestRepository extends JpaRepository<Interest, UUID>, InterestRepositoryCustom {
+
+    @Query("""
+            SELECT DISTINCT i
+            FROM Interest i
+            LEFT JOIN FETCH i.keywords
+            """)
+    List<Interest> findAllWithKeywords();
 
 }
