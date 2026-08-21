a5b23b818d4b5e463569af464dc17669972823b8
줴령 <gpfud09@gmail.com>
2026-08-21 11:33:28 +0900

    feat: 기사 단건 조회용 toDto 메서드 추가

--- src/main/java/com/codeit/monew/article/mapper/ArticleMapper.java
+++ src/main/java/com/codeit/monew/article/mapper/ArticleMapper.java
@@ -21,6 +21,15 @@ public interface ArticleMapper {
     @Mapping(source = "viewCount", target = "viewCount")
     ArticleDto toDto(ArticleSearchResult result);
 
+    @Mapping(source = "article.id", target = "id")
+    @Mapping(source = "article.source", target = "source")
+    @Mapping(source = "article.sourceUrl", target = "sourceUrl")
+    @Mapping(source = "article.title", target = "title")
+    @Mapping(source = "article.publishDate", target = "publishDate")
+    @Mapping(source = "article.summary", target = "summary")
+    @Mapping(source = "viewedByMe", target = "viewedByMe")
+    ArticleDto toDto(Article article, Long commentCount, Long viewCount, Boolean viewedByMe);
+
     List<ArticleDto> toDtoList(List<ArticleSearchResult> articles);
 
 }
