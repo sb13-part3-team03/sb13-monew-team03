1f56d21ccc404ad35e1e8c9a1da79ec1aa4f499e
marcel615 <alswo000615@gmail.com>
2026-08-21 16:46:10 +0900

    fix: 코드 리뷰 수정

--- src/test/java/com/codeit/monew/interest/service/InterestServiceTest.java
+++ src/test/java/com/codeit/monew/interest/service/InterestServiceTest.java
@@ -21,6 +21,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -37,6 +38,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.never;
 
 @ExtendWith(MockitoExtension.class)
@@ -591,16 +593,22 @@ class InterestServiceTest {
             );
 
             // then
-            then(articleInterestRepository)
-                    .should()
+            InOrder inOrder = inOrder(
+                    interestRepository,
+                    articleInterestRepository,
+                    subscriptionRepository
+            );
+
+            inOrder.verify(interestRepository)
+                    .findById(interestId);
+
+            inOrder.verify(articleInterestRepository)
                     .deleteAllByInterest_Id(interest.getId());
 
-            then(subscriptionRepository)
-                    .should()
+            inOrder.verify(subscriptionRepository)
                     .deleteAllByInterestId(interest.getId());
 
-            then(interestRepository)
-                    .should()
+            inOrder.verify(interestRepository)
                     .delete(interest);
         }
 
