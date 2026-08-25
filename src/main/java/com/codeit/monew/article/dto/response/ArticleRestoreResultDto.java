6c451f4f95215ade2030e2048f3d7c0845cc4d0f
줴령 <gpfud09@gmail.com>
2026-08-25 18:13:21 +0900

    refactor: restoredArticleIds 타입 UUID로 변경

--- src/main/java/com/codeit/monew/article/dto/response/ArticleRestoreResultDto.java
+++ src/main/java/com/codeit/monew/article/dto/response/ArticleRestoreResultDto.java
@@ -2,10 +2,11 @@ package com.codeit.monew.article.dto.response;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.UUID;
 
 public record ArticleRestoreResultDto (
         Instant restoreDate,
-        List<String> restoredArticleIds,
+        List<UUID> restoredArticleIds,
         Long restoredArticleCount
 ) {
 }
