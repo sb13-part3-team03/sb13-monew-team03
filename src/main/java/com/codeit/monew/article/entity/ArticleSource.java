28931492bf7a74cf78aa7affff61051a9ce4d302
줴령 <gpfud09@gmail.com>
2026-08-28 18:00:05 +0900

    fix: 기존 YONHAP enum 값 호환 처리

--- src/main/java/com/codeit/monew/article/entity/ArticleSource.java
+++ src/main/java/com/codeit/monew/article/entity/ArticleSource.java
@@ -1,5 +1,6 @@
 package com.codeit.monew.article.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -11,6 +12,16 @@ public enum ArticleSource {
     HANKYUNG("한국경제"),
     CHOSUN("조선일보"),
     YEONHAP("연합뉴스");
+
+    @JsonCreator
+    public static ArticleSource from(String value) {
+        if ("YONHAP".equals(value)) {
+            // 기존 YONHAP으로 저장된 데이터와의 호환성을 유지
+            return YEONHAP;
+        }
+
+        return ArticleSource.valueOf(value);
+    }
 
     private final String description;
 
