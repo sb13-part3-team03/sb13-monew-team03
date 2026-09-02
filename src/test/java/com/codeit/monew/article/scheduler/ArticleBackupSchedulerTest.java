bc58d7d7e2ea2b2b913f5e85fad88a718896cde7
줴령 <gpfud09@gmail.com>
2026-09-02 15:39:33 +0900

    test: ArticleBackupScheduler 테스트 설정 수정

--- src/test/java/com/codeit/monew/article/scheduler/ArticleBackupSchedulerTest.java
+++ src/test/java/com/codeit/monew/article/scheduler/ArticleBackupSchedulerTest.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
@@ -17,7 +17,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 public class ArticleBackupSchedulerTest {
 
     @Mock
